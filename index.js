eba3f621f72d2c5a9f4c7c177af8c750230efe3e
Roziboy04 <fxolboyev007@gmail.com>
2023-03-31 18:09:04 +0300

    siuuuuuuuuuu

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// 5
+
+// const a = +prompt("1-sonni kriting")
+// const b = +prompt("2-sonni kriting")
+// const c = +prompt("3-soni kriting")
+
+// function num(a, b, c,) {
+//   return (a * b * c)
+// }
+
+// console.log(num(a, b, c,));
+
+
+// 6
+
+
+
+// function nav(n,b) {
+//   if (n<b) {
+//       return "<"
+//   }else if (n>b){
+//       return ">" 
+//   }else{
+//       return "="
+//   }
+// }
+// console.log(nav([5],[7]));
+
+
